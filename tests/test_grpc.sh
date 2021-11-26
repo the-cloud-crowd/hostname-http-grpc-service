b1d926ea30d07fa2d19dfe5a3a68e09e38da4ad3
Thomas Gard <gard@semvox.de>
2021-11-26 15:26:03 +0100

    Adapt proto file path in test script

--- tests/test_grpc.sh
+++ tests/test_grpc.sh
@@ -4,6 +4,6 @@ TARGET=${1:-"-plaintext localhost:50051"}
 
 grpcurl  -H 'X-UserId: john' \
   -d '{"sender":"john"}' \
-  -import-path hostname-proto/src/main/proto/hostname \
+  -import-path $(dirname $0)/../hostname-proto/src/main/proto/hostname \
   -proto hostname.proto \
   $TARGET "hostname.HostnameService/GetHostname"
